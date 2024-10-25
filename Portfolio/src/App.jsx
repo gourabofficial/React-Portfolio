50a9cd619827eebb12de6cd180c1f3290f07861d
gourabofficial <gangulygourab3@gmail.com>
2024-10-25 22:39:59 +0530

    Add .gitignore file and initial project structure

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <h1 className="text-white">React portfoloio</h1>
+    </>
+  );
+};
+
+export default App;
