18a590e9705a719fdf23f6957259c384e5f6d178
gourabofficial <gangulygourab3@gmail.com>
2024-11-10 19:59:04 +0530

    refactor: enhance event handling in App component for better user experience

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -10,44 +10,43 @@ import Github from './Pages/Github';
 
 export const App = () => {
   
-  // useEffect(() => {
-  //   // Disable right-click
-  //   const handleContextMenu = (event) => {
-  //     event.preventDefault();
-  //   };
+  useEffect(() => {
+    // Disable right-click
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+    };
 
-  //   // Disable inspect element and developer tools
-  //   const handleKeyDown = (event) => {
-  //     if (
-  //       (event.ctrlKey && event.shiftKey && event.key === 'I') ||
-  //       (event.ctrlKey && event.shiftKey && event.key === 'J') ||
-  //       (event.ctrlKey && event.key === 'U') ||
-  //       (event.key === 'F12')
-  //     ) {
-  //       event.preventDefault();
-  //     }
-  //   };
+    // Disable inspect element and developer tools
+    const handleKeyDown = (event) => {
+      if (
+        (event.ctrlKey && event.shiftKey && event.key === 'I') ||
+        (event.ctrlKey && event.shiftKey && event.key === 'J') ||
+        (event.ctrlKey && event.key === 'U') ||
+        (event.key === 'F12')
+      ) {
+        event.preventDefault();
+      }
+    };
 
-  //   // Disable dragging of images
-  //   const handleDragStart = (event) => {
-  //     if (event.target.tagName === 'IMG') {
-  //       event.preventDefault();
-  //     }
-  //   };
+    // Disable dragging of images
+    const handleDragStart = (event) => {
+      if (event.target.tagName === 'IMG') {
+        event.preventDefault();
+      }
+    };
 
-  //   // Add event listeners
-  //   document.addEventListener('contextmenu', handleContextMenu);
-  //   document.addEventListener('keydown', handleKeyDown);
-  //   document.addEventListener('dragstart', handleDragStart);
+    // Add event listeners
+    document.addEventListener('contextmenu', handleContextMenu);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('dragstart', handleDragStart);
 
-  //   // Cleanup function to remove event listeners
-  //   return () => {
-  //     document.removeEventListener('contextmenu', handleContextMenu);
-  //     document.removeEventListener('keydown', handleKeyDown);
-  //     document.removeEventListener('dragstart', handleDragStart);
-  //   };
-  // }, []); // Empty dependency array to run only once on mount
-
+    // Cleanup function to remove event listeners
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('dragstart', handleDragStart);
+    };
+  }, []);
   return (
     <>
       <main className="bg-gradient-to-r from-blue-950 to-purple-900
